8453ebf7d6a673156e06d52a7400fc539e9b4857
raynald <raynaldchung@gmail.com>
2015-01-19 11:18:34 +0100

    extract commit

--- extract.py
+++ extract.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
         line = line.strip()
         sample += [line]
     random.shuffle(sample)
-    for i in range(len(sample)/100+1, len(sample)/100+len(sample)/100*9):
+    for i in range(len(sample)/10+1, len(sample)/10+len(sample)/10*5):
         print sample[i]
 
